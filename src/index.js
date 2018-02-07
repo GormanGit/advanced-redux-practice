6e4ca0590f796f1438676a5c773e6cfa56eceeec
MrUser <user1@localhost.localdomain>
2018-02-07 16:51:46 -0600

    final

--- src/index.js
+++ src/index.js
@@ -6,30 +6,9 @@ import state from "./state";
 import store from "./store";
 import {Provider} from "react-redux";
 
-// const {
-//   dateTime,
-//   newComments,
-//   newTasks,
-//   newOrders,
-//   // tickets,
-//   orders,
-//   taskItem,
-//   tasks,
-//   messages
-// } = state;
 
 ReactDOM.render(
   <Provider store={store}>
-    <App
-      // taskItem={taskItem}
-      // dateTime={dateTime}
-      // newComments={newComments}
-      // newTasks={newTasks}
-      // newOrders={newOrders}
-      // tickets={tickets}
-      // orders={orders}
-      // messages={messages}
-      // tasks={tasks}
-    /></Provider>,
+    <App /></Provider>,
   document.getElementById("root")
 );
